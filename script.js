b9f824ef207a31e938471b5c073080fcba721da2
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-10-28 08:39:49 -0400

    Write pseudocode, base case, thoughts and first attempts at recursive fibonacci

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ function fibs(num) {
 }
 
 fibs(8);
+
+// Fibonacci Sequence - Recursive Version
+
+// Pseudocode:
+
+// start with empty arr and num, ex. 8
+// reduce the num by 1 and 2 for each iteration and use result fo calculate next val in array
+// push new val to array
+
+// changed to reduce the num by 1 for each iteration after re-reading instructions
+// fn() should only need to run 8 times, so only count down by 1
+
+// base case: hit zero
+// when you reach num == 0 push num at index 0
+// when you reach num == 1 push num at index 1
+
+// Thoughts
+// should be thinking in indexes rather then sum
+// as the stack is traversed back up, use num for index
+
+// 1st few attempts:
+// function fibsRec(num, arr = []) {
+//   console.log("this was printed recursively");
+//   if (num == 0 || num == 1) {
+//     return arr.push(num);
+//   } else {
+//     arr.push(fibsRec(num - 2) + fibsRec(num - 1));
+//     //arr.push( // result ot fibsRec returning arr.index(num-2) + result of sibsRec returning arr.index(num-1))
+//     // let value = fibsRec(num - 2, arr) + fibsRec(num - 1, arr);
+//     // arr.push(value);
+//   }
+// }
