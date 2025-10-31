6c7257bc802330155f6149ff849a9fb1658d6e27
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-10-31 16:03:19 -0400

    Create a new fn() to do a linear comparison of left and right arrays. Test and add to recursive function. Also, fix syntax error in arr.slice

--- script.js
+++ script.js
@@ -5,11 +5,11 @@
 const arr = [3, 2, 1, 13, 8, 5, 0, 1];
 
 // arr split tests
-const midPoint = Math.floor(arr.length / 2);
-console.log(arr);
-console.log(midPoint);
-console.log(arr.slice(0, midPoint));
-console.log(arr.slice(midPoint, arr.length));
+// const midPoint = Math.floor(arr.length / 2);
+// console.log(arr);
+// console.log(midPoint);
+// console.log(arr.slice(0, midPoint));
+// console.log(arr.slice(midPoint, arr.length));
 
 // use Math.floor to handle arrs with an odd # of values
 // remember that splice mutates the original arr, but slice does not
@@ -28,20 +28,52 @@ console.log(arr.slice(midPoint, arr.length));
 // when one list becomes empty, copy all values from the remaining arr into the sorted arr
 
 function mergeSort(arr) {
+  console.log(`This function was called recursively with ${arr}`);
   if (arr.length <= 1) {
     return arr;
   }
   const midPoint = Math.floor(arr.length / 2);
-  const leftArr = mergeSort(arr.slice(0 - midPoint));
+  console.log(`leftArr sliced at midpoint: ${arr.slice(0, midPoint)}`);
+  const leftArr = mergeSort(arr.slice(0, midPoint));
+  console.log(
+    `rightArr sliced at midpoint: ${arr.slice(midPoint, arr.length)}`
+  );
   const rightArr = mergeSort(arr.slice(midPoint, arr.length));
-
-  // compare 1st element of each arr
-  // push smallest value to a new arr
-  // continue until end of both arrs are reached
-  // is one arr is empty, just copy values from the other arr (the values are already sorted by this point)
+  return compare(leftArr, rightArr);
 }
 
-// mergeSort(arr);
+mergeSort(arr);
+
+function compare(a, b) {
+  const sortedArr = [];
+  let i = 0;
+  let j = 0;
+  while (i < a.length && j < b.length) {
+    if (a[i] <= b[j]) {
+      sortedArr.push(a[i]);
+      i++;
+    }
+    if (a[i] > b[j]) {
+      sortedArr.push(b[j]);
+      j++;
+    }
+  }
+  while (i < a.length) {
+    sortedArr.push(a[i]);
+    i++;
+  }
+  while (j < b.length) {
+    sortedArr.push(b[j]);
+    j++;
+  }
+  console.log(`Values compared and placed in sorted order: ${sortedArr}`);
+  return sortedArr;
+}
+
+// const arr2 = [2, 4, 5, 7];
+// const arr3 = [1, 6, 4];
+
+// compare(arr2, arr3);
 
 // Steps (left side):
 // 1: Take arr: [3, 2, 1, 13, 8, 5, 0, 1], divide by 2 and get vals from 0 to midpoint > arr is now [3, 2, 1, 13]
